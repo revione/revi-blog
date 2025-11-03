009d6f49bbfffbe54c623e843b79f6c98089173f
revione <revione@outlook.com>
2025-11-03 16:10:50 +0100

    Fix: Convertir a string primero

--- components/query-pagination.tsx
+++ components/query-pagination.tsx
@@ -28,7 +28,7 @@ export function QueryPagination({
   const nextPage = currentPage + 1
 
   const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(searchParams)
+    const params = new URLSearchParams(searchParams.toString())
     params.set("page", pageNumber.toString())
     return `${pathname}?${params.toString()}`
   }
